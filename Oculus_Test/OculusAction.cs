c1d58178175a671f5364240e37176d8d048e1ffa
Mebius <viddafroygeo@hotmail.com>
2015-06-18 15:43:58 +0200

    fix fail indent + load library on the super class for OculusActions

--- Oculus_Test/OculusAction.cs
+++ Oculus_Test/OculusAction.cs
@@ -4,22 +4,47 @@ using Oculus_Test.Utils;
 
 namespace Oculus_Test
 {
-    public class OculusAction
+  public class OculusAction
+  {
+    public static IntPtr HGetProcIddll;
+
+    public OculusAction(string dllVersion)
     {
-        public IntPtr HGetProcIddll;
+      switch (dllVersion)
+      {
+        case "New":
+          HGetProcIddll = Dll.LoadLibrary(@"C:\Users\Casque2\OneDrive\Stage IFSTTAR\Oculus_Test\Oculus_Test\Oculus (DX11).dll");
+          Console.WriteLine(@"HGetProcIddll vaut: " + HGetProcIddll);
+          break;
+        case "Old":
+          HGetProcIddll = Dll.LoadLibrary(@"C:\Users\Casque2\OneDrive\Stage IFSTTAR\Oculus_Test\Oculus_Test\Oculus.dll");
+          Console.WriteLine(@"HGetProcIddll vaut: " + HGetProcIddll);
+          break;
+        case "None":
+          // By default, when no version is specified, we will use the old one
+          HGetProcIddll = Dll.LoadLibrary(@"C:\Users\Casque2\OneDrive\Stage IFSTTAR\Oculus_Test\Oculus_Test\Oculus.dll");
+          Console.WriteLine(@"HGetProcIddll vaut: " + HGetProcIddll);
+          break;
+      }
 
-        public OculusAction()
-        {
-            HGetProcIddll = Dll.LoadLibrary("Oculus (DX11).dll");
-            if (HGetProcIddll == null)
-            {
-                Console.WriteLine(Resources.OculusAction_OculusAction_Error_could_not_load_the_dynamic_library);
-            }
-        }
-
-        public string Show()
-        {
-            throw new NotImplementedException();
-        }
+      if (HGetProcIddll == IntPtr.Zero)
+      {
+        Console.WriteLine(Resources.OculusAction_OculusAction_Error_could_not_load_the_dynamic_library);
+      }
     }
+
+    public bool IsDllLoad()
+    {
+      return (HGetProcIddll != IntPtr.Zero);
+    }
+
+    public string Show()
+    {
+      throw new NotImplementedException();
+    }
+
+    protected OculusAction()
+    {
+    }
+  }
 }

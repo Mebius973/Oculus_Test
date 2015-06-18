a616d101152f58d7c04477c866e167a5103ce280
Mebius <viddafroygeo@hotmail.com>
2015-06-18 15:47:24 +0200

    implem for OculusActions's init

--- Oculus_Test/OculusActions/Init.cs
+++ Oculus_Test/OculusActions/Init.cs
@@ -1,7 +1,44 @@
-﻿namespace Oculus_Test.OculusActions
+﻿using System;
+using System.Runtime.InteropServices;
+using Oculus_Test.Properties;
+using Oculus_Test.Utils;
+
+namespace Oculus_Test.OculusActions
 {
-    public class Init : OculusAction
+  public class Init : OculusAction
+  {
+    private string _status;
+
+    public Init(string dllVersion)
+      : base(dllVersion)
     {
-         
+      _status = "Initializing";
+      InitializeOculus();
     }
+
+    public string Show()
+    {
+      return _status;
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate int Initialize();
+
+    private IntPtr RetrieveDllInitFunction()
+    {
+      var init = Dll.GetProcAddress(HGetProcIddll, "birth");
+      if (init != IntPtr.Zero) return init;
+      Console.WriteLine(Resources.Init_RetrieveDllInitFunction_Error__init_function_not_found_in_dll);
+      _status = "Oculus' init function not found in dll,, we drifted into the rift!";
+      throw new NullReferenceException();
+    }
+
+    private void InitializeOculus()
+    {
+      var init = RetrieveDllInitFunction();
+      var initializeOculus = (Initialize)Marshal.GetDelegateForFunctionPointer(init, typeof(Initialize));
+      initializeOculus();
+      _status = "Oculus might be ready";
+    }
+  }
 }

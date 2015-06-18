82efff437b5f2aeadbacfb4482b5c1a8c9c36709
Mebius <viddafroygeo@hotmail.com>
2015-06-18 15:45:13 +0200

    fix fail indent

--- Oculus_Test/Utils/Dll.cs
+++ Oculus_Test/Utils/Dll.cs
@@ -3,16 +3,16 @@ using System.Runtime.InteropServices;
 
 namespace Oculus_Test.Utils
 {
-    public static class Dll
-    {
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr LoadLibrary(string dllToLoad);
+  public static class Dll
+  {
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr LoadLibrary(string dllToLoad);
 
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
 
 
-        [DllImport("kernel32.dll")]
-        public static extern bool FreeLibrary(IntPtr hModule);
-    }
+    [DllImport("kernel32.dll")]
+    public static extern bool FreeLibrary(IntPtr hModule);
+  }
 }

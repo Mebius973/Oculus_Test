f75a5915c0a8f71d30abc21772b9b0d03464c371
Mebius <viddafroygeo@hotmail.com>
2015-06-18 15:48:19 +0200

    implem to get access to the eye image height

--- Oculus_Test/OculusActions/EyeHeight.cs
+++ Oculus_Test/OculusActions/EyeHeight.cs
@@ -1,30 +1,40 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using Oculus_Test.Properties;
 using Oculus_Test.Utils;
 
 namespace Oculus_Test.OculusActions
 {
-    public class EyeHeight : OculusAction
+  public class EyeHeight : OculusAction
+  {
+    private int _height;
+
+    public EyeHeight(string dllVersion)
     {
-        private int _height;
-
-        public EyeHeight()
-        {
-            Ask();
-        }
-
-        public new string Show()
-        {
-            return _height.ToString();
-        }
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate int GetImageHeight();
-        private void Ask()
-        {
-            var eyeHeight = Dll.GetProcAddress(HGetProcIddll, "getImageHeight");
-            var getImageHeight = (GetImageHeight)Marshal.GetDelegateForFunctionPointer(eyeHeight, typeof(GetImageHeight));
-            _height = getImageHeight();
-        }
+      Ask();
     }
+
+    public new string Show()
+    {
+      return _height.ToString();
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate int GetImageHeight();
+
+    private static IntPtr RetrieveDllGetImageHeightFunction()
+    {
+      var eyeHeight = Dll.GetProcAddress(HGetProcIddll, "getImageHeight");
+      if (eyeHeight != IntPtr.Zero) return eyeHeight;
+      Console.WriteLine(Resources.EyeHeight_Ask_Error__can_t_find_the_getImageHeight_function_from_the_dll);
+      throw new NullReferenceException();
+    }
+
+    private void Ask()
+    {
+      var eyeHeight = RetrieveDllGetImageHeightFunction();
+      var getImageHeight = (GetImageHeight)Marshal.GetDelegateForFunctionPointer(eyeHeight, typeof(GetImageHeight));
+      _height = getImageHeight();
+    }
+  }
 }

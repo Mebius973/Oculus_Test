1fba46d22fb479669935932ac3c4a1e9f552d816
Mebius <viddafroygeo@hotmail.com>
2015-06-25 17:25:08 +0200

    Implem for Proceed OculusAction

--- Oculus_Test/OculusActions/Proceed.cs
+++ Oculus_Test/OculusActions/Proceed.cs
@@ -1,10 +1,44 @@
-﻿namespace Oculus_Test.OculusActions
+﻿using System;
+using System.Runtime.InteropServices;
+using Oculus_Test.Properties;
+using Oculus_Test.Utils;
+
+namespace Oculus_Test.OculusActions
 {
-    public class Proceed : OculusAction
+  public class Proceed : OculusAction
+  {
+    private string _status;
+
+    public Proceed(string dllVersion)
+      : base(dllVersion)
     {
-      public Proceed(string dllVersion)
-        : base(dllVersion)
-        {
-        }
+      _status = "Beginning precessing Oculus";
+      ProcessOculus();
     }
+
+    public new string Show()
+    {
+      return _status;
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate int Process();
+
+    private IntPtr RetrieveDllProcessFunction()
+    {
+      var process = Dll.GetProcAddress(HGetProcIddll, "process");
+      if (process != IntPtr.Zero) return process;
+      Console.WriteLine(Resources.Proceed_RetrieveDllProcessFunction_RetrieveDllInitFunction_Error__init_function_not_found_in_dll);
+      _status = "Oculus' process function not found in dll, we drifted into the rift!";
+      throw new NullReferenceException();
+    }
+
+    private void ProcessOculus()
+    {
+      var process = RetrieveDllProcessFunction();
+      var processOculus = (Process)Marshal.GetDelegateForFunctionPointer(process, typeof(Process));
+      processOculus();
+      _status = "Oculus has been proceed";
+    }
+  }
 }

41ae0614e55e5fef6ff9e62122f3bacbcaf5b64b
Mebius <viddafroygeo@hotmail.com>
2015-06-25 17:27:17 +0200

    Implem for Tracking OculusAction

--- Oculus_Test/OculusActions/Tracking.cs
+++ Oculus_Test/OculusActions/Tracking.cs
@@ -1,10 +1,41 @@
-﻿namespace Oculus_Test.OculusActions
+﻿using System;
+using System.Runtime.InteropServices;
+using Oculus_Test.Properties;
+using Oculus_Test.Utils;
+
+namespace Oculus_Test.OculusActions
 {
     public class Tracking : OculusAction
     {
+      private float[] _tracking;
+
       public Tracking(string dllVersion)
         : base(dllVersion)
         {
+          Ask();
+        }
+
+        public new string Show()
+        {
+          return _tracking.ToString();
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate float[] GetTracker();
+
+        private static IntPtr RetrieveDllGetTrackerFunction()
+        {
+          var tracker = Dll.GetProcAddress(HGetProcIddll, "getTracker");
+          if (tracker != IntPtr.Zero) return tracker;
+          Console.WriteLine(Resources.Tracking_RetrieveDllGetTrackerFunction_Ask_Error__can_t_find_the_getTracker_function_from_the_dll);
+          throw new NullReferenceException();
+        }
+
+        private void Ask()
+        {
+          var tracker = RetrieveDllGetTrackerFunction();
+          var retrieveTracking = (GetTracker)Marshal.GetDelegateForFunctionPointer(tracker, typeof(GetTracker));
+          _tracking = retrieveTracking();
         }
     }
 }

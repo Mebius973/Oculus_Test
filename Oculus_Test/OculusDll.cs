d211cba9e4232dcc2c03b1f65ddc3ec5340d8de8
Mebius <viddafroygeo@hotmail.com>
2015-06-29 19:00:47 +0200

    add function to handle the non multiple instance of init or proceed and handle the release part (not working though: releasing then init again => crash)

--- Oculus_Test/OculusDll.cs
+++ Oculus_Test/OculusDll.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Oculus_Test.OculusActions;
 using Oculus_Test.Utils;
 
 namespace Oculus_Test
@@ -6,16 +7,39 @@ namespace Oculus_Test
   static class OculusDll
   {
     private static IntPtr _dllPtr;
+    private static string _currentVersion;
+    private static bool _isInitialzed;
+    private static bool _isProceed;
 
     public static IntPtr Load(string dllVersion)
     {
-      if (_dllPtr != IntPtr.Zero)
+      if (_dllPtr != IntPtr.Zero && _currentVersion != dllVersion)
       {
-       var test = Dll.FreeLibrary(_dllPtr);
+        var test = Dll.FreeLibrary(_dllPtr);
         Console.WriteLine(test);
       }
       _dllPtr = SelectVersion(dllVersion);
+      _currentVersion = dllVersion;
       return _dllPtr;
+    }
+
+    public static void InitializeOculus(string dllVersion)
+    {
+      if (_currentVersion == dllVersion && _isInitialzed) return;
+      var init = new Init(dllVersion);
+      _isInitialzed = init.IsInitialized();
+    }
+
+    public static void ProcessOculus(string dllVersion)
+    {
+      if (_currentVersion == dllVersion && _isProceed) return;
+      var proceed = new Proceed(dllVersion);
+      _isProceed = proceed.IsProceed();
+    }
+
+    public static void ReleaseOculus()
+    {
+      _isInitialzed = false;
     }
 
     private static IntPtr SelectVersion(string dllVersion)

9255d84247aee761c7910c48b2b34c7bae3c9547
Mebius <viddafroygeo@hotmail.com>
2015-07-09 14:22:23 +0200

    some refactor / code reorganisation to clean Proceed code and come back to less SRP violations

--- Oculus_Test/OculusActions/Proceed.cs
+++ Oculus_Test/OculusActions/Proceed.cs
@@ -46,97 +46,34 @@ namespace Oculus_Test.OculusActions
       return Image.FromFile(side == "left" ? @"C:\Users\Casque2\OneDrive\Stage IFSTTAR\Oculus_Test\Oculus_Test\assets\pirate-ship-left-eye.bmp" : @"C:\Users\Casque2\OneDrive\Stage IFSTTAR\Oculus_Test\Oculus_Test\assets\pirate-ship-right-eye.bmp");
     }
 
-    private byte[] BmpToBytes(Image source)
-    {
-      var converter = new ImageConverter();
-      var bytes = (byte[])converter.ConvertTo(source, typeof(byte[]));
-      if (bytes != null) return bytes;      
-        _status = "Couldn't open image files";
-      throw new ArgumentNullException();
-    }
-
-    private void ConvertRgbToRgba(byte[] sourceRgb, byte[] targetRgba)
-    {
-      if (targetRgba.Length / sourceRgb.Length != 4 / 3)
-      { 
-        throw new ArgumentException();
-      }
-      var target = new byte[4];
-      for (var i = 0; i < sourceRgb.Length / 3; i++)
-      {
-        target[2] = sourceRgb[3 * i];
-        target[1] = sourceRgb[3 * i + 1];
-        target[0] = sourceRgb[3 * i + 2];
-        target[3] = 0;
-
-        target.CopyTo(targetRgba, 4 * i);
-      }
-    }
-
     private unsafe void ProcessOculus()
     {
-      //TODO: Oculus Params which should be somewhere else
-      var RGBMODE = 3;
-      var RGBAMODE = 4;
-      var renderWidth = 2360;
-      var sourceRgbRenderWidth = RGBMODE * renderWidth;
-      var rgbRenderWidth = RGBMODE * renderWidth + 0;
-      var rgbaRenderWidth = RGBAMODE * renderWidth + 0;
-      var renderHeight = 1460;
-      var sizeRGB = renderHeight * rgbRenderWidth;
-      var sizeRGBA = renderHeight * rgbaRenderWidth;
-
-      // rgb and rgba byte[] used to load the image from the bmp and then preparing it to be transfered to the Oculus.
-      byte[] rgbDataBytes = new byte[sizeRGB] ;
-      for (int i = 0; i < rgbDataBytes.Length; i++) { rgbDataBytes[i] = 0; }
-      byte[] rgbaDataBytes = new byte[sizeRGBA];
-      for (int i = 0; i < rgbaDataBytes.Length; i++) { rgbaDataBytes[i] = 0; }
-
       // Retrieving the Process function from the dll
       var process = RetrieveDllProcessFunction();
       var processOculus = (Process)Marshal.GetDelegateForFunctionPointer(process, typeof(Process));
 
       // Left eye image
       var bmp = GetImage("left");
-      var leftDataBytes = BmpToBytes(bmp);
-
-      Console.Write("2 * leftDataBytes.Length - sizeRGB: ");
-      Console.WriteLine(2 * leftDataBytes.Length - sizeRGB);
+      var leftDataBytes = Bytes.BmpToBytes(bmp); // TODO: try/catch the exception raised by BmpToBytes
 
       // Right eye image
       bmp = GetImage("right");
-      var rightDataBytes = BmpToBytes(bmp);
-      //We juxtapose the images
+      var rightDataBytes = Bytes.BmpToBytes(bmp); // TODO: try/catch the exception raised by BmpToBytes
 
-      var maxIteration = 0;
-      for (var y = 0; y < renderHeight; y++)
-      {
-        for (var x = 0; x < rgbRenderWidth; x++)
-        {
-          if (x < sourceRgbRenderWidth / 2)
-          {
-            rgbDataBytes[x + ((renderHeight - 1 - y) * rgbRenderWidth)] = leftDataBytes[x + (y * sourceRgbRenderWidth / 2)];
-          }
-          else if ((rgbRenderWidth - x) < sourceRgbRenderWidth / 2)
-          {
-            rgbDataBytes[x + ((renderHeight - 1 - y) * rgbRenderWidth)] = rightDataBytes[x - (rgbRenderWidth - (sourceRgbRenderWidth / 2)) + (y * sourceRgbRenderWidth / 2)];
-          }
-          else 
-          {
-            rgbDataBytes[x + ((renderHeight - 1 - y) * rgbRenderWidth)] = 0;
-          }
-          maxIteration++;
-        }
-      }
+      // We juxtapose the images
+      byte[] rgbDataBytes = Bytes.EyeImagesToOculusRgb(leftDataBytes, rightDataBytes);
 
-      ConvertRgbToRgba(rgbDataBytes, rgbaDataBytes);
+      // Then we convert to RGBA
+      byte[] rgbaDataBytes = Bytes.ConvertRgbToRgba(rgbDataBytes);
+
+      // Finally displays everything
       MainWindow.SetLeftEyeImage(leftDataBytes);
       MainWindow.SetRightEyeImage(rightDataBytes);
       fixed (byte* bytes = rgbaDataBytes)
       {
         processOculus(bytes);
       }
-   
+
       _status = "Oculus has been proceed";
       _isProceed = true;
     }
---
+++ Oculus_Test/Utils/Bytes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Oculus_Test.Utils
+{
+  static class Bytes
+  {
+    public static int RGBMODE = 3;
+    public static int RGBAMODE = 4;
+    private static int renderWidth = 2360;
+    private static int renderHeight = 1460;
+    private static int rgbRenderWidth = RGBMODE * renderWidth;
+    private static int rgbaRenderWidth = RGBAMODE * renderWidth;
+    public static int sizeRGB = renderHeight * rgbRenderWidth;
+    private static int sizeRGBA = renderHeight * rgbaRenderWidth;
+
+    public static byte[] BmpToBytes(Image source)
+    {
+      var converter = new ImageConverter();
+      var target = (byte[])converter.ConvertTo(source, typeof(byte[]));
+      if (target != null) return target;
+      throw new ArgumentNullException();
+    }
+
+    public static byte[] ConvertRgbToRgba(byte[] sourceRgb)
+    {
+      var targetRgba  = new byte[sizeRGBA];
+      for (int i = 0; i < targetRgba.Length; i++) { targetRgba[i] = 0; }
+      if (targetRgba.Length / sourceRgb.Length != 4 / 3)
+      {
+        throw new ArgumentException();
+      }
+      var target = new byte[4];
+      for (var i = 0; i < sourceRgb.Length / 3; i++)
+      {
+        target[2] = sourceRgb[3 * i];
+        target[1] = sourceRgb[3 * i + 1];
+        target[0] = sourceRgb[3 * i + 2];
+        target[3] = 0;
+
+        target.CopyTo(targetRgba, 4 * i);
+      }
+      return targetRgba;
+    }
+
+    public static byte[] EyeImagesToOculusRgb(byte[] leftDataBytes, byte[] rightDataBytes)
+    {
+      var rgbDataBytes = new byte[sizeRGB] ;
+      for (int i = 0; i < rgbDataBytes.Length; i++) { rgbDataBytes[i] = 0; }
+      for (var y = 0; y < renderHeight; y++)
+      {
+        for (var x = 0; x < rgbRenderWidth; x++)
+        {
+          if (x < rgbRenderWidth / 2)
+          {
+            rgbDataBytes[x + ((renderHeight - 1 - y) * rgbRenderWidth)] = leftDataBytes[x + (y * rgbRenderWidth / 2)];
+          }
+          else if ((rgbRenderWidth - x) < rgbRenderWidth / 2)
+          {
+            rgbDataBytes[x + ((renderHeight - 1 - y) * rgbRenderWidth)] = rightDataBytes[x - (rgbRenderWidth - (rgbRenderWidth / 2)) + (y * rgbRenderWidth / 2)];
+          }
+          else
+          {
+            rgbDataBytes[x + ((renderHeight - 1 - y) * rgbRenderWidth)] = 0;
+          }
+        }
+      }
+      return rgbDataBytes;
+    }
+  }
+}
